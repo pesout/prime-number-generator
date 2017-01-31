66c3e462d327cbd95ad68940a72324d2d194417c
Štěpán Pešout <stepan@pesout.eu>
2017-01-31 22:09:28 +0100

    Update prvocisla.js

--- prvocisla.js
+++ prvocisla.js
@@ -5,3 +5,35 @@ function vypis(x)
       document.getElementById("vystup").innerHTML = x;
     }
 }
+
+function prvo(x)
+{
+
+
+  y = x.toString().charAt((x.toString()).length-1);
+
+  if (y==0 || y==2 || y==4 || y==5 || y==6 || y==8)
+  {
+    vypis(0);
+  }
+  else {
+    odmo = Math.sqrt(x);
+    test = 3;
+    delit = 1;
+
+    while (test<=odmo && delit!=0)
+    {
+      delit = x % test;
+      test = test + 2;
+    }
+
+    if(delit==0)
+    {
+      vypis(0);
+    }
+    else
+    {
+      vypis(x);
+    }
+  }
+}
